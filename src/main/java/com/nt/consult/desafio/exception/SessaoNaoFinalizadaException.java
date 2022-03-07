84653821d3ad016f5db35a520e30ad432ebccc90
Marcio Serafim <marciofilho2@gmail.com>
2022-03-07 11:50:23 -0300

    Atualizando status de retorno de algumas exceções que não estavam certas

--- src/main/java/com/nt/consult/desafio/exception/SessaoNaoFinalizadaException.java
+++ src/main/java/com/nt/consult/desafio/exception/SessaoNaoFinalizadaException.java
@@ -3,7 +3,7 @@ package com.nt.consult.desafio.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
 public class SessaoNaoFinalizadaException extends RuntimeException {
 
 	public SessaoNaoFinalizadaException() {
